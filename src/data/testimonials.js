0013c84464a6de33a5993d3c3bca9be7c83cf812
Rohit Bansode <rohitbansode.tech@gmail.com>
2025-12-03 13:51:15 +0530

    solve name missmatch issue

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,11 +1,11 @@
 export const testimonials = [
     {
-        name: "Ankit Sharma",
+        name: "Yash Gote",
         role: "Project Manager, SETTribe LLP",
         feedback: "Rohit delivered complex frontend modules on time and maintained high-quality code standards. Great problem solver and communicator."
     },
     {
-        name: "Vivek Baranwal",
+        name: "Suryakant Baranwal",
         role: "CEO, Baranwal Consultancy",
         feedback: "He understood the requirements quickly and implemented secure authentication and dashboards efficiently."
     }
